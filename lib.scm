022e0208a30178aa77c6f1e4caabc8b001f13664
chiayolin <chiayo.lin@gmail.com>
2017-08-26 19:31:01 +0700

    add ++ / -- operators

--- lib.scm
+++ lib.scm
@@ -1,15 +1,11 @@
 (begin
-  
-  (define #t 1)
-  (define #f 0)
   (define nil (quote ()))
   
   (define null? (lambda (lst) (if (eq? lst nil) #t #f)))
 
   (define map
     (lambda (fn lst)
-      (if (null? lst)
-        lst
+      (if (null? lst) lst 
         (cons (fn (car lst)) (map fn (cdr lst))))))
 
   (define not (lambda (a)   (if a #f #t)))
@@ -17,8 +13,12 @@
   (define and (lambda (a b) (if a  b #f)))
   (define xor (lambda (a b) (or (and (not a) b) (and a (not b)))))
 
-  (define neg (lambda (n) (- 0 x)))
-  (define abs (lambda (n) (if (<= n 0) (neg n) n)))
-  (define !   (lambda (n) (if (= n 1) 1 (* n (! (- n 1))))))
+  (define ++  (lambda (n)   (+ n 1)))
+  (define --  (lambda (n)   (- n 1)))
+  (define neg (lambda (n)   (- 0 n)))
+  (define abs (lambda (n)   (if (<= n 0) (neg n) n)))
+  
+  (define pow (lambda (b e) (if (= e 1) b (* b (pow b (-- e))))))
+  (define !   (lambda (n)   (if (= n 1) 1 (* n (! (-- n))))))
 
 )
