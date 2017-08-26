c38d5f86e01435de602532698ea55656ce196a77
chiayolin <chiayo.lin@gmail.com>
2017-08-26 17:00:54 +0700

    logic and map library functions added

--- lib.scm
+++ lib.scm
@@ -2,9 +2,23 @@
   
   (define #t 1)
   (define #f 0)
+  (define nil (quote ()))
   
-  (define neg (lambda (x) (- 0 x)))
-  (define abs (lambda (x) (if (<= x 0) (neg x) x)))
-  (define !   (lambda (x) (if (= x 1) 1 (* x (! (- x 1))))))
+  (define null? (lambda (lst) (if (eq? lst nil) #t #f)))
+
+  (define map
+    (lambda (fn lst)
+      (if (null? lst)
+        lst
+        (cons (fn (car lst)) (map fn (cdr lst))))))
+
+  (define not (lambda (a)   (if a #f #t)))
+  (define or  (lambda (a b) (if a #t  b)))
+  (define and (lambda (a b) (if a  b #f)))
+  (define xor (lambda (a b) (or (and (not a) b) (and a (not b)))))
+
+  (define neg (lambda (n) (- 0 x)))
+  (define abs (lambda (n) (if (<= n 0) (neg n) n)))
+  (define !   (lambda (n) (if (= n 1) 1 (* n (! (- n 1))))))
 
 )
